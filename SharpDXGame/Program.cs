88fac9346b567bebb4816b987a6d702f583a07be
treanisieg <egor.anisimov@valmetpartners.com>
2016-04-08 09:29:45 +0300

    Github repo intialization

---
+++ SharpDXGame/Program.cs
@@ -0,0 +1,17 @@
+﻿// See
+// http://web.archive.org/web/20140625070410/http://rastertek.com/tutindex.html
+// https://rastertekdx.codeplex.com/
+
+namespace SharpDXGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
